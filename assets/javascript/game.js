e7846322b20c2c76860c12a091252eadb95e39f7
Kate Foust <kate.e.foust@gmail.com>
2019-12-15 22:50:03 -0600

    added true/false; all letters returning false currently

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -5,13 +5,19 @@ var words = [
     "santa",
     "snowflake",
     "sleigh"];
+
+//user guess
+var keyInput;    
     
 var wins=0;
 
 var guesses= 12;
 
+var guess;
+
 //array to store guessed letters
-var lettersGuessed = [];
+var correctGuess = [];
+var incorrectGuess = [];
 
 //diplay wins
 
@@ -24,18 +30,39 @@ console.log(chosenWord)
 
 
 //Number of dashes same length as random word. 
-var dashes = "";
+var dashes = " ";
 for (i = 0; i < chosenWord.length; i++) {
     if (chosenWord.charAt(i) == " ") {
       dashes += " ";
     } else {
       dashes += "__ ";
     }
-  }
-//display dashes
-  document.getElementById("current-word").innerHTML=dashes;
+    //display dashes
+    document.getElementById("current-word").innerHTML=dashes;
+    console.log(dashes);
 
-console.log(dashes);
+}
+
+
+//Capture letters player presses on keyboard.
+
+document.onkeyup = function keyPress(event){
+    keyInput = event.key;
+    lettersGuessed=keyInput.split(" ");
+    var found = false;
+    for (i=0; i < chosenWord.length; i++){
+        if (guess === chosenWord[i]) {
+            correctGuess[i] = guess;
+            document.getElementById("current-word").innerHTML=lettersGuessed;
+            found=true;
+        }
+    }
+    if (found) return;
+
+console.log(found);
+}
+
+
 
 //Check to see if player presses a valid letter.
 
@@ -52,17 +79,6 @@ console.log(guesses);
 
 
 
-//Capture letters player presses on keyboard.
-
-document.onkeyup = function keyPress(event){
-    var keyInput = event.key;
-    var lettersGuessed=keyInput.split(" ");
-
-document.getElementById("current-word").innerHTML=lettersGuessed;
-console.log(lettersGuessed);
-
-    console.log(keyInput);
-}
 
 
 
