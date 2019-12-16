48ac9f290fee5e669d59e54ceae2b49c3a336a95
Kate Foust <kate.e.foust@gmail.com>
2019-12-16 00:39:00 -0600

    final submission for hw

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -13,7 +13,9 @@ var keyInput;
 
 var wins=0;
 
+
 var guesses= 12;
+document.getElementById("guesses").innerHTML=guesses;
 
 //array to store guessed letters
 var correctGuess = [];
@@ -26,26 +28,34 @@ console.log(wins); //works
 
 //Pick Random Word from list (math.random)
 var chosenWord = words[Math.floor(Math.random() * words.length)];
+//When player wins, add to win score.
+
 
 console.log(chosenWord); //works
 
 
 //Number of dashes same length as random word. 
+//broken:when letter is true, all dashes disappear
+function hideWord() {
 var dashes = " ";
 for (i = 0; i <= chosenWord.length; i++) {
     if (chosenWord.charAt(i) == " ") {
       dashes += " ";
-    } else {
+    } 
+    else {
       dashes += "__ ";
     }
     //display dashes
     document.getElementById("current-word").innerHTML=dashes;
-    console.log(dashes); //works
+    console.log(dashes);
+}
 
 }
+hideWord();
 
 
 //Capture letters player presses on keyboard.
+//Check to see if player presses a valid letter.
 
 function checkLetter(){
 document.onkeyup = function(event){
@@ -64,28 +74,30 @@ console.log(found);
 
     console.log(found);
 
+//Keep track of incorrect letters guessed.
 
     if(incorrectGuess.indexOf(keyInput) <0) {
         incorrectGuess.push(keyInput);
-        document.getElementById("letters-guessed").innerHTML = incorrectGuess.join(" ");
+        document.getElementById("letters-guessed").innerHTML = incorrectGuess.join(" ");    
+    }
+    
+    //If player guesses invalid letter, remove number of tries by 1 (12 tries total).
 
-}
+    if (found = false){
+        guesses--;
+        console.log(guesses);
+    }//broken
+
 }
 }
 checkLetter();
 
-
-
-//Check to see if player presses a valid letter.
-
-//Keep track of letters guessed.
-
-//If player guesses valid letter, replace dash with correct letter.
-
-
-//If player guesses invalid letter, remove number of tries by 1 (12 tries total).
-
-
-//When player wins, add to win score.
+//coded out because it's broken function win(){
+  //  if (correctGuess===chosenWord.length){
+  //game resets  
+  //    wins++;
+    //}
+//}
+//win();
 
 
