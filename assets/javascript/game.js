1bd8ede6335ea53b615a6d06687918c77ca1feee
Kate Foust <kate.e.foust@gmail.com>
2019-12-18 20:33:55 -0600

    added losses; wins/losses works; number of guesses works

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -6,16 +6,16 @@ var words = [
     "snowflake",
     "sleigh"];
 
-    console.log(words); //works
+console.log(words); //works
 
 //user guess
-var keyInput;    
+var keyInput;
 
-var wins=0;
+var wins = 0;  // is for every game ///you can play several times and wins and losses increment based on when you win or lost
+var losses = 0;
 
-
-var guesses= 12;
-document.getElementById("guesses").innerHTML=guesses;
+var guesses;
+var chosenWord;
 
 //array to store guessed letters
 var correctGuess = [];
@@ -23,73 +23,89 @@ var incorrectGuess = [];
 
 //diplay wins
 
-document.getElementById("wins").innerHTML= wins;
+document.getElementById("wins").innerHTML = wins;
 console.log(wins); //works
 
-//Pick Random Word from list (math.random)
-var chosenWord = words[Math.floor(Math.random() * words.length)];
-//When player wins, add to win score.
 
-
-console.log(chosenWord); //works
 
 
 //Number of dashes same length as random word. 
 //broken:when letter is true, all dashes disappear
-function hideWord() {
-    for (i = 0; i < chosenWord.length; i++) {
-      correctGuess[i] = "__";
-    }
-  
-    document.getElementById("current-word").innerHTML = correctGuess.join(" ");
-    console.log(correctGuess);
-  }
+
 
 //Capture letters player presses on keyboard.
 //Check to see if player presses a valid letter.
 
-function checkLetter(){
-document.onkeyup = function(event){
+
+document.onkeyup = function (event) {
     keyInput = event.key.toLowerCase();
+    console.log("key", keyInput)
     var found = false;
-    for (i=0; i <= chosenWord.length; i++){
+    for (i = 0; i < chosenWord.length; i++) {
         if (keyInput === chosenWord[i]) {
             correctGuess[i] = keyInput;
-            document.getElementById("current-word").innerHTML=keyInput =correctGuess.join(" ");
-            found=true;
-console.log(found);
+            found = true;
+            console.log(found, correctGuess);
         }
 
     }
-    if (found) return;
+    document.getElementById("current-word").innerHTML = correctGuess.join(" ");
 
-    console.log(found);
-
-//Keep track of incorrect letters guessed.
-
-    if(incorrectGuess.indexOf(keyInput) <0) {
-        incorrectGuess.push(keyInput);
-        document.getElementById("letters-guessed").innerHTML = incorrectGuess.join(" ");    
+    if (found) {
+        if (correctGuess.indexOf("__") < 0) {
+            wins++
+            document.getElementById("wins").innerHTML = wins;
+            console.log("you won:", wins)
+            init()
+        }
     }
-    
-    //If player guesses invalid letter, remove number of tries by 1 (12 tries total).
+    else {
+        if (incorrectGuess.indexOf(keyInput) < 0) {
+            incorrectGuess.push(keyInput);
+            document.getElementById("letters-guessed").innerHTML = incorrectGuess.join(" ");
+            guesses--;
+            document.getElementById("guesses").innerHTML = guesses;
 
-    if (found = false){
-        guesses--;
-        console.log(guesses);
-    }//broken
+            if (guesses === 0) {
+                losses++
+                document.getElementById("losses").innerHTML = losses;
+                console.log("lost", losses)
+                init()
+            }
+        }
+
+    }
 
 }
+
+
+
+
+function init() {
+    // new word
+    // restart the guesses counter 12
+    // show the guesses on the DOM
+
+
+    //Pick Random Word from list (math.random)
+    chosenWord = words[Math.floor(Math.random() * words.length)];
+    console.log(chosenWord)
+    //When player wins, add to win score.
+    guesses = 12;  // current round
+    document.getElementById("guesses").innerHTML = guesses;
+
+
+    console.log(chosenWord); //works
+
+    for (i = 0; i < chosenWord.length; i++) {
+        correctGuess[i] = "__";
+    }
+    document.getElementById("current-word").innerHTML = correctGuess.join(" ");
+
+    incorrectGuess = []
+    document.getElementById("letters-guessed").innerHTML = incorrectGuess.join(" ");
+
 }
-hideWord();
-checkLetter();
-
-//coded out because it's broken function win(){
-  //  if (correctGuess===chosenWord.length){
-  //game resets  
-  //    wins++;
-    //}
-//}
-//win();
 
 
+init()
